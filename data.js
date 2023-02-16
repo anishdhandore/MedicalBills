62b9148833ed13215182eaaaf06e885980540c90
Anish Dhandore <anish.dhandore@gmail.com>
2023-02-15 22:01:12 -0800

    created a GET request to fetch all the data

--- data.js
+++ data.js
@@ -1,23 +1,21 @@
-const medicalBills = [
+var medicalBills = [
     {
-        Name : "Kyle Rawshon",
-        Address : "659 S Twin Oaks Valley Road",
+        name : "Kyle Rawshon",
+        address : "659 S Twin Oaks Valley Road",
         hospitalName: "TexasHealth",
         serviceDate: "02/29/2022",
         amount: "1202.3"
     },
-
     {
-        Name : "Ronnie Nicole",
-        Address : "122 Parkway Inn",
+        name : "Ronnie Nicole",
+        address : "122 Parkway Inn",
         hospitalName: "Visat Mathew Hospital",
         serviceDate: "01/15/2023",
         amount: "450"
     },
-
     {
-        Name : "Lucky Ali",
-        Address : "A12 udaymitra cooperative housing",
+        name : "Lucky Ali",
+        address : "A12 udaymitra cooperative housing",
         hospitalName: "Sai Hospital",
         serviceDate: "02/10/2023",
         amount: "998"
--- home.js
+++ home.js
@@ -1,14 +1,19 @@
 const express = require('express')
+const data = require("./data.js")
 app = express()
 
 app.set('view engine', 'ejs')
 // allows us to use information from forms
 app.use(express.urlencoded({extended: true}))
 
+// on a get request, also send the medical bills)
 app.get('/', (req, res) => {
     // res.sendStatus(500)
     // res.send("Hello World!")
-    res.render('home.ejs')
+    // show all the bills here 
+    // res.send(JSON.stringify(data))
+    console.log(data)
+    res.render('home.ejs', {data: data})
 })
 
 app.listen(3000)
